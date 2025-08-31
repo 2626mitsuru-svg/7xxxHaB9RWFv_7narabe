daa3cde4a7cce5166fdc30d7e7c242438d98de2f
2626mitsuru-svg <2626mitsuru@gmail.com>
2025-08-31 22:06:10 +0900

    vite修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 // vite.config.ts
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'; // ★必須（default import）
+import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
